c8d4fad97485f74e40b0a365cf5f65837bc399ec
Fights On <fights.on433@gmail.com>
2018-02-22 10:59:43 -1000

    Added Kali w/ OpenVAS script

---
+++ kali_openvas.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# For use with Kali with OpenVAS installed.
+
+# Installation:
+
+#   #> cp kali_openvas.sh /usr/bin/update
+#   #> chmod +x /usr/bin/update
+#   #> update
+
+# ===== Shouldn't have to change anything under this =====
+
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+printf "${BLUE}System Updater${NC}\n\n"
+printf "${GREEN}[+] Checking for root:${NC}"
+if [ $(sudo id -u) = 0 ]; then
+    printf "${GREEN} OK${NC}\n"
+    printf "${GREEN}[+] Updating Kali:${NC}\n"
+    sudo apt-get update | sed 's/^/    /'
+    sudo apt-get dist-upgrade -y | sed 's/^/    /'
+    sudo apt-get autoremove -y | sed 's/^/    /'
+    sudo apt-get autoclean -y | sed 's/^/    /'
+    sudo apt-get clean -y | sed 's/^/    /'
+
+printf "${GREEN}[+] Updating OpenVAS:${NC}\n"
+    sudo openvas-stop | sed 's/^/    /'
+    sudo openvasmd --update && sudo openvasmd --rebuild
+
+printf "${YELLOW}\n[!] Would you like to reboot? [y|N] ${NC}"
+read -r response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    sudo reboot
+fi
+else
+    printf "\n${RED}[!] YOU MUST BE ROOT!${NC}\n"
+fi
