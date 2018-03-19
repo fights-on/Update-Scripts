1089249a531e2793c664dfebeb355566ff0a4d80
Fights On <fights.on433@gmail.com>
2018-03-19 10:13:13 -1000

    Created Centos Update Script

---
+++ centos.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# For use with REMnux.
+
+# Installation:
+
+#   #> cp centos.sh /usr/bin/update
+#   #> chmod +x /usr/bin/update
+#   #> update
+
+# ===== Shouldn't have to change anything under this =====
+
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+printf "${BLUE}System Updater${NC}\n\n"
+printf "${GREEN}[+] Checking for root:${NC}"
+if [ $(sudo id -u) = 0 ]; then
+    printf " OK\n"
+
+printf "${GREEN}[+] Updating the REMnux system:${NC}\n"
+    sudo yum update -y | sed 's/^/    /'
+    sudo yum autoremove -y | sed 's/^/    /'
+    sudo yum clean all | sed 's/^/    /'
+    sudo rm -rf /var/cache/yum
+
+printf "${YELLOW}\n[!] Would you like to reboot? [y|N] ${NC}"
+read -r response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    sudo reboot
+fi
+else
+    printf "\n${RED}[!] YOU MUST BE ROOT!${NC}\n"
+fi
