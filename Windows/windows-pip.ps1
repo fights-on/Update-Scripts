435917945b4d349034eaec28486d04ea5c05fc2d
Fights On <fights.on433@gmail.com>
2018-03-23 20:38:29 -1000

    Workon around for pip failing to update

--- Windows/windows-pip.ps1
+++ Windows/windows-pip.ps1
@@ -4,10 +4,13 @@ Function Update-PIP {
     $CurrentPrincipal = New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
     If ($CurrentPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
         Write-Host "OK"
+        Write-Host "[+] Downloading/Running Get-PIP.py: " -ForegroundColor "Green"
+        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py" -OutFile "$($env:Temp)\get-pip.py"
+        python.exe "$($env:Temp)\get-pip.py"
         Write-Host "[+] Checking for PIP: " -NoNewline -ForegroundColor "Green"
         If (Get-Command "pip.exe" -ErrorAction SilentlyContinue){
             Write-Host "OK"
-            Write-Host "[+] Updating PIP: " -ForegroundColor "Green"
+            Write-Host "[+] Updating Python Packages: " -ForegroundColor "Green"
             pip.exe list | Select-Object -Skip 2 | %{ $_.Split()[0]; } | %{pip.exe install --upgrade $_}
             Write-Host "[+] Done" -ForegroundColor "Green"
         } Else {
